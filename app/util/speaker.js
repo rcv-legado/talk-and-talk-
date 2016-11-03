d129d46a9b3c98a2da026dc9e9e79cad188afaf3
Rafael Guinho Monteiro <rafaelguinho@outlook.com>
2016-11-03 12:48:05 -0300

    Fixed some bugs

--- app/util/speaker.js
+++ app/util/speaker.js
@@ -1,14 +1,18 @@
 class Speaker {
     static speak(text) {
 
-        let msg = new SpeechSynthesisUtterance();
-        msg.voiceURI = 'native';
-        msg.volume = 1;
-        msg.rate = 0.6;
-        msg.pitch = 1;
+        window.speechSynthesis.onvoiceschanged = function () {
+            let msg = new SpeechSynthesisUtterance();
+            msg.voiceURI = 'native';
+            msg.voice = speechSynthesis.getVoices()[4];
+            msg.volume = 1;
+            msg.rate = 0.9;
+            msg.pitch = 1;
 
-        msg.lang = 'en-US';
-        msg.text = text;
-        speechSynthesis.speak(msg);
+            msg.lang = 'en-US';
+            msg.text = text;
+            speechSynthesis.speak(msg);
+
+        }
     }
 }
