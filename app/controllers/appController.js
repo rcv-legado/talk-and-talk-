0c7aa4bc439072f6d023877f3c3a3755aab8590d
Rafael Guinho Monteiro <rafaelguinho@outlook.com>
2016-11-04 12:46:35 -0300

    Included new gifs

--- app/controllers/appController.js
+++ app/controllers/appController.js
@@ -1,21 +1,34 @@
 app.controller('AppCtrl', function ($scope, $mdToast) {
 
-    let allPhases = ["What airline am I flying?", 
-    "Where is the restroom?", 
-    "How much does the magazine cost?", 
-    "May I purchase headphones?",
-    "How do I access the Internet?"
+    let allPhases = ["What airline am I flying?",
+        "Where is the restroom?",
+        "How much does the magazine cost?",
+        "May I purchase headphones?",
+        "How do I access the Internet?",
+        "Where can I find a restaurant?",
+        "I have lost my passport.",
+        "Someone stole my money.",
+        "Where is the hospital?",
+        "Where can I find a grocery store?",
+        "My room is messy, and I would like it cleaned.",
+        "I would like two double beds, please.",
+        "How many beds are in the room?",
+        "Do you know where this hotel is?"
     ];
 
     $scope.determinateValue = 0;
-    const step = 100/allPhases.length;
+    const step = 100 / allPhases.length;
 
     function initParameters() {
-        
+
         $scope.error = false;
         $scope.understood = '';
         $scope.fail = false;
         $scope.success = false;
+    }
+
+    $scope.readPhase = function (currentPhase) {
+        Speaker.speak(currentPhase);
     }
 
     $scope.init = function () {
@@ -31,21 +44,17 @@ app.controller('AppCtrl', function ($scope, $mdToast) {
 
     $scope.init();
 
-    $scope.showSimpleToast = function(msg) {
-    
-    $mdToast.show(
-      $mdToast.simple()
-        .textContent(msg)
-        .hideDelay(4000)
-    );
-  };
+    $scope.showSimpleToast = function (msg) {
 
-    $scope.readPhase = function (currentPhase) {
-        Speaker.speak(currentPhase);
-    }
+        $mdToast.show(
+            $mdToast.simple()
+                .textContent(msg)
+                .hideDelay(4000)
+        );
+    };
 
     $scope.startRecognition = function (event, currentPhase) {
-        
+
         $scope.listening = true;
         initParameters();
 
@@ -57,11 +66,11 @@ app.controller('AppCtrl', function ($scope, $mdToast) {
 
             var similarity = Similarity.getSimilarity($scope.understood, $scope.currentPhase);
 
-            if (isSimilar(similarity)){
+            if (isSimilar(similarity)) {
                 $scope.success = true;
                 $scope.determinateValue += step;
 
-                if($scope.determinateValue >= 100) $scope.showSimpleToast('You did!');
+                if ($scope.determinateValue >= 100) $scope.showSimpleToast('You did!');
             }
             else
                 $scope.fail = true;
--- app/util/speaker.js
+++ app/util/speaker.js
@@ -2,9 +2,12 @@ class Speaker {
     static speak(text) {
 
         window.speechSynthesis.onvoiceschanged = function () {
+
+            var voices = window.speechSynthesis.getVoices();
+
             let msg = new SpeechSynthesisUtterance();
             msg.voiceURI = 'native';
-            msg.voice = speechSynthesis.getVoices()[4];
+            msg.voice = voices[4];
             msg.volume = 1;
             msg.rate = 0.9;
             msg.pitch = 1;
@@ -13,6 +16,7 @@ class Speaker {
             msg.text = text;
             speechSynthesis.speak(msg);
 
+            return;
         }
     }
 }
